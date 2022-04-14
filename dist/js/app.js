28cdee487400c62b2a70809813f44d5eb192dd01
DESKTOP-OAK1LA4\Made Sudiarta <made.sudiarta220595@gmail.com>
2022-04-14 17:49:52 +0800

    Adding dark Mode

--- dist/js/app.js
+++ dist/js/app.js
@@ -10,10 +10,45 @@ hamburger.addEventListener('click', function(){
 window.onscroll = function(){
   const header = document.querySelector('header');
   const FixedNav = header.offsetTop;
+  const toTop = document.querySelector('#to-top');
 
   if(window.pageYOffset > FixedNav) {
     header.classList.add('navbar-fixed');
+    toTop.classList.remove('hidden');
+    toTop.classList.add('flex');
   }else{
     header.classList.remove('navbar-fixed');
+    toTop.classList.remove('flex');
+    toTop.classList.add('hidden');
   }
+};
+
+// Klik diluar Hamburger
+window.addEventListener('click', function(e){ 
+  if(e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+  }
+});
+
+// Dark Mode Toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function(){
+  if(darkToggle.checked){
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  }else{
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+  }
+});
+
+// Posisi toggle sesuai mode
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
 };
